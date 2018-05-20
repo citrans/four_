0899336513f381c993c05dd3c9bb236e8bd546c0
DESKTOP-9KODVNA\Citra <citransa8@gmail.com>
2018-05-21 04:35:44 +0700

    coba membuat getignore

---
+++ Konveksi/app/src/main/java/com/example/citra/konveksi/plama.java
@@ -0,0 +1,13 @@
+package com.example.citra.konveksi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class plama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plama);
+    }
+}
---
+++ Konveksi/app/src/main/java/com/example/citra/konveksi/produk_konveksi.java
@@ -0,0 +1,13 @@
+package com.example.citra.konveksi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class produk_konveksi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produk_konveksi);
+    }
+}
